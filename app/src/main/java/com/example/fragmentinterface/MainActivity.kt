d3bea9504eec7e12afffa966403ccbc58797c814
Flackooooo <batzorig11@gmail.com>
2019-05-17 10:45:36 +0800

    Initial commit

---
+++ app/src/main/java/com/example/fragmentinterface/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.progressbar
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.example.fragmentinterface.R
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity(), HomeFragment.OnFragmentInteractionListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragment_container, HomeFragment())
+            .commit()
+    }
+    override fun onStartProgressBar() {
+        progressBar.visibility = View.VISIBLE
+    }
+
+    override fun onStopProgressBar() {
+        progressBar.visibility = View.GONE
+    }
+}
